70aaec31d4003140f73156148b85b84d4fd96274
Jessie <jess.bell@me.com>
2017-09-15 16:45:42 -0700

    remove unintended characters located in formula, move code into proper alignment

--- js/scripts.js
+++ js/scripts.js
@@ -8,28 +8,28 @@ $(document).ready(function() {
       if (userScore > 4 && userScore < 8) {
         $("#answer").text(name + ", you should take the CSS/Design track!");
         $("#design").removeClass();
- +      $("#description").text("You will learn CSS, HTML, and JavaScript to learn how to style web pages using complex, responsive layouts. You will learn front-end development to control what the user of a website can see and do.");
+        $("#description").text("You will learn CSS, HTML, and JavaScript to learn how to style web pages using complex, responsive layouts. You will learn front-end development to control what the user of a website can see and do.");
       }
       else if (userScore > 9 && userScore < 13) {
         $("#answer").text(name + ", you should take the SQL track!");
         $("#sql").removeClass();
- +      $("#description").text("Structured Query Language (SQL) is used to communicate with databases. Its a bit different than some of the others offered, as you can't build a website or app using SQL, SQL is used to build and manage the databases apps and websites use.");
+        $("#description").text("Structured Query Language (SQL) is used to communicate with databases. Its a bit different than some of the others offered, as you can't build a website or app using SQL, SQL is used to build and manage the databases apps and websites use.");
       }
       else if (userScore > 14 && userScore < 16) {
         $("#answer").text(name + ", you should take the Ruby & Rails track!");
         $("#ruby").removeClass();
- +      $("#description").text("Ruby is a beginner-friendly and dynamic language. It powers Ruby on Rails, which is a framework used to quickly develop sites and applications. Ruby is used on sites like Twitter and Shopify.");
+        $("#description").text("Ruby is a beginner-friendly and dynamic language. It powers Ruby on Rails, which is a framework used to quickly develop sites and applications. Ruby is used on sites like Twitter and Shopify.");
       }
       else if (userScore > 17 && userScore < 19 ) {
         $("#answer").text(name + ", you should take the C#! and .NET track");
         $("#csharp").removeClass();
- +      $("#description").text("C# is popular amongst larger, well-established companies. It has Microsoft backing. If you like the idea of doing back-end coding for a larger business, C# is a great language to learn.")
+        $("#description").text("C# is popular amongst larger, well-established companies. It has Microsoft backing. If you like the idea of doing back-end coding for a larger business, C# is a great language to learn.")
 
       }
       else {
         $("#answer").text(name + ", you should take the Java/Android track!");
         $("#android").removeClass();
- +      $("#description").text("Android is used in everything from phones to tablets to watches. You will learn Java and JavaScript along with the Android framework to develop mobile apps.");
+        $("#description").text("Android is used in everything from phones to tablets to watches. You will learn Java and JavaScript along with the Android framework to develop mobile apps.");
       }
 
       $("#track").hide();
