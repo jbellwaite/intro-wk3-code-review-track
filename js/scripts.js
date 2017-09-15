24e66ab302b059d59e4c21140dea0ce2ebc3f23a
Jessie <jess.bell@me.com>
2017-09-15 11:12:13 -0700

    add values to radio button items, change values in branch logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,22 @@ $(document).ready(function() {
      parseInt($("input:radio[name=friends]:checked").val()) +
      parseInt($("input:radio[name=freetime]:checked").val()) + parseInt($("input:radio[name=why]:checked").val()) ;
 
-      if (userScore === 5 || userScore === 6) {
+      if (userScore > 4 && userScore < 8) {
         $("#answer").text(name + ", you should take the Design track!");
         $("#design").removeClass();
  +      $("#description").text("You will learn CSS, HTML, and JavaScript to learn how to style web pages using complex, responsive layouts. You will learn front-end development to control what the user of a website can see and do.");
       }
-      else if (userScore === 7 || userScore === 8) {
+      else if (userScore > 9 && userScore < 13) {
         $("#answer").text(name + ", you should take the SQL track!");
         $("#sql").removeClass();
- +      $("#description").text("Structured Query Language (SQL) is used to communicate with databases. Its a bit different than some of the others listed, as you can't build a website or app using SQL, SQL is used to build and manage the databases apps and websites use.");
+ +      $("#description").text("Structured Query Language (SQL) is used to communicate with databases. Its a bit different than some of the others offered, as you can't build a website or app using SQL, SQL is used to build and manage the databases apps and websites use.");
       }
-      else if (userScore === 9) {
+      else if (userScore > 14 && userScore < 16) {
         $("#answer").text(name + ", you should take the Ruby & Rails track!");
         $("#ruby").removeClass();
  +      $("#description").text("Ruby is a beginner-friendly and dynamic language. It powers Ruby on Rails, which is a framework used to quickly develop sites and applications. Ruby is used on sites like Twitter and Shopify.");
       }
-      else if (userScore === 10 || userScore === 12) {
+      else if (userScore > 17 && userScore < 19 ) {
         $("#answer").text(name + ", you should learn C#!");
         $("#csharp").removeClass();
  +      $("#description").text("C# is popular amongst larger, well-established companies. It has Microsoft backing. If you like the idea of doing back-end coding for a larger business, C# is a great language to learn.")
